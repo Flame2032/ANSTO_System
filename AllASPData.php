5cf84ccf0c2a3f440dcb61f0d8f4248ca5ad4205
jonoclarke <38773785+jonoclarke@users.noreply.github.com>
2018-06-05 22:33:22 +1000

    Add files via upload

--- AllASPData.php
+++ AllASPData.php
@@ -52,7 +52,7 @@
 		
         <!--Dropdown filter bar-->
         <div class = 'filterBar' id = "filterBar">
-            <form action = "AllData.php" method = 'POST'>
+            <form action = "AllASPData.php" method = 'POST'>
                 <!--Filter by Searchbox section-->
                 <div class = 'row'>
                     <input type = 'checkbox' class = 'searchByCheckbox' name = 'column'><p class = 'searchBarText'>Filter by Search:</p>
@@ -68,31 +68,51 @@
                     <input type = 'checkbox' class = 'searchByCheckbox' name = 'column'><p class = 'searchBarText'>By Columns:</p>
                 </div>
                 <div class = 'container-white'>
-					<input type="checkbox" name="filter[]" value ="aspID"/> aspID<br>
-                    <input type = 'checkbox' name="filter[]" value ="Site"/>Site<br>
-					<input type = 'checkbox' name="filter[]" value ="Exposure Date"/>Exposure Date<br>
-					<input type = 'checkbox' name="filter[]" value ="Type"/>Type<br>
-                    <input type = 'checkbox' name="filter[]" value ="Pre Filter Mass"/>Pre Filter Mass<br>
-                    <input type = 'checkbox' name="filter[]" value ="Pre Laser"/>Pre Laser<br>
-                    <input type = 'checkbox' name="filter[]" value ="Date"/>Date<br>
-                    <input type = 'checkbox' name="filter[]" value ="Pre Vacuum"/>Pre Vacuum<br>
-                    <input type = 'checkbox' name="filter[]" value ="Post Mass"/>Post Mass<br>
-                    <input type = 'checkbox' name="filter[]" value ="Post Laser"/>Post Laser<br>
-                    <input type = 'checkbox' name="filter[]" value ="Post Vacuum"/>Post Vacuum<br>
-                    <input type = 'checkbox' name="filter[]" value ="Post Mass Volume"/>Post Mass Volume<br>
-                    <input type = 'checkbox' name="filter[]" value ="FinishFlowRate"/>Finish Flow Rate<br>
-                    <input type = 'checkbox' name="filter[]" value ="ElapsedTime"/>Elapsed Time<br>
-                    <input type = 'checkbox' name="filter[]" value ="TemperatureMax"/>Temperature Max<br>
-                    <input type = 'checkbox' name="filter[]" value ="TemperatureMin"/>Temperature Min<br>
-                    <input type = 'checkbox' name="filter[]" value ="Comments"/>Comments<br>
-                    <input type = 'checkbox' name="filter[]" value ="PreQADate"/>Pre QA Date<br>
-                    <input type = 'checkbox' name="filter[]" value ="SentDate"/>Sent Date<br>
-                    <input type = 'checkbox' name="filter[]" value ="ReturnDate"/>Return Date<br>
-                    <input type = 'checkbox' name="filter[]" value ="ReturnQADate"/>Return QA Date<br>
-                    <input type = 'checkbox' name="filter[]" value ="ExposureDate"/>Exposure Date<br>
-                    <input type = 'checkbox' name="filter[]" value ="FilterType"/>Filter Type<br>
-                    <input type = 'checkbox' name="filter[]" value ="PreMABI"/>Pre-MABI<br>
-                    <input type = 'checkbox' name="filter[]" value ="PostMABI"/>Post-MABI<br>
+					<input type="checkbox" name="filter[]" value ="`aspID`"/>aspID<br>
+                    <input type = 'checkbox' name="filter[]" value ="`Site`"/>Site<br>
+					<input type = 'checkbox' name="filter[]" value ="`Exposure Date`"/>Exposure Date<br>
+					<input type = 'checkbox' name="filter[]" value ="`Type`"/>Type<br>
+                    <input type = 'checkbox' name="filter[]" value ="`Pre Filter Mass`"/>Pre Filter Mass<br>
+                    <input type = 'checkbox' name="filter[]" value ="`Pre Laser`"/>Pre Laser<br>
+                    <input type = 'checkbox' name="filter[]" value ="`Date`"/>Date<br>
+                    <input type = 'checkbox' name="filter[]" value ="`Time`"/>Time<br>
+                    <input type = 'checkbox' name="filter[]" value ="`Initials`"/>Initials<br>
+					<input type = 'checkbox' name="filter[]" value ="`Vacuum Pinch Tube`"/>Vacuum Pinch Tube<br>
+					<input type = 'checkbox' name="filter[]" value ="`Magnehelic`"/>Magnehelic<br>
+					<input type = 'checkbox' name="filter[]" value ="`Post Laser`"/>Post Laser<br>
+                    <input type = 'checkbox' name="filter[]" value ="`Vacuum`"/>Vacuum<br>
+                    <input type = 'checkbox' name="filter[]" value ="`Post Filter Mass`"/>Post Filter Mass<br>
+					<input type = 'checkbox' name="filter[]" value ="`TemperatureMax`"/>Temperature Max<br>
+                    <input type = 'checkbox' name="filter[]" value ="`TemperatureMin`"/>Temperature Min<br>
+                    <input type = 'checkbox' name="filter[]" value ="`Comments`"/>Comments<br>
+					<input type = 'checkbox' name="filter[]" value ="`Date2`"/>Date2<br>
+                    <input type = 'checkbox' name="filter[]" value ="`Time2`"/>Time2<br>
+                    <input type = 'checkbox' name="filter[]" value ="`Initials3`"/>Initials3<br>
+					<input type = 'checkbox' name="filter[]" value ="`Vacuum4`"/>Vacuum4<br>
+                    <input type = 'checkbox' name="filter[]" value ="`ReturnDate`"/>Return Date<br>
+				    <input type = 'checkbox' name="filter[]" value ="`Magnehelic5`"/>Magnehelic5<br>
+                    <input type = 'checkbox' name="filter[]" value ="`Elapsed Time`"/>Elapsed Time<br>
+                    <input type = 'checkbox' name="filter[]" value ="`PostDate`"/>PostDate<br>
+                    <input type = 'checkbox' name="filter[]" value ="`QA`"/>QA<br>
+                    <input type = 'checkbox' name="filter[]" value ="`QR barcode Information`"/>QR barcode Information<br>
+					<input type = 'checkbox' name="filter[]" value ="`Exposure Day`"/>Exposure Day<br>
+                    <input type = 'checkbox' name="filter[]" value ="`Exposure Month`"/>Exposure Month<br>
+					<input type = 'checkbox' name="filter[]" value ="`Exposure Year`"/>Exposure Year<br>
+                    <input type = 'checkbox' name="filter[]" value ="`SamplingDay`"/>SamplingDay<br>
+					<input type = 'checkbox' name="filter[]" value ="`I0 (405`"/>I0 (405 <br>
+					<input type = 'checkbox' name="filter[]" value ="`I0 (465)`"/>I0 (465)<br>
+                    <input type = 'checkbox' name="filter[]" value ="`I0 (525)`"/>I0 (525)<br>
+                    <input type = 'checkbox' name="filter[]" value ="`I0 (639)`"/>I0 (639)<br>
+					<input type = 'checkbox' name="filter[]" value ="`I0 (870)`"/>I0 (870)<br>
+                    <input type = 'checkbox' name="filter[]" value ="`I0 (940)`"/>I0 (940)<br>
+					<input type = 'checkbox' name="filter[]" value ="`I0 (1050)`"/>I0 (1050)<br>
+                    <input type = 'checkbox' name="filter[]" value ="`I (405)`"/>I (405)<br>
+					<input type = 'checkbox' name="filter[]" value ="`I (465)`"/>I (465)<br>
+                    <input type = 'checkbox' name="filter[]" value ="`I (525)`"/>I (525)<br>
+					<input type = 'checkbox' name="filter[]" value ="`I (639)`"/>I (639)<br>
+                    <input type = 'checkbox' name="filter[]" value ="`I (870)`"/>I (870)<br>
+					<input type = 'checkbox' name="filter[]" value ="`I (940)`"/>I (940)<br>
+					<input type = 'checkbox' name="filter[]" value ="`I (1050)`"/>I (1050)<br>
 					
                 </div>
                 <div class = 'filterSeparator'></div>
@@ -120,28 +140,54 @@
                         <div class = 'filterSeparator'></div>";
                     }
                 ?>
-                <input type="submit" class = "btn-ansto" value = "ApplyFilterOptions" method ="POST" style = "width:100%;">
+                <input type="submit" class = "btn-ansto" name="search" value = "ApplyFilterOptions" method ="POST" style = "width:100%;">
 				 
                 
             </form>
 			
         </div>
 
-<?php
+ <?php
 if(isset($_POST['filter'])) {
  $chkbox = $_POST['filter'];
  $i = 0;
 $sql_where = implode (" , ", $chkbox);
  While($i < sizeof($sql_where))
  {
-$result = "SELECT  $sql_where FROM asp WHERE `Exposure Date` >= now()-interval 3 month " ;
  $i++;
 
 
  
  }
+ $sql = "SELECT $sql_where FROM asp WHERE `Exposure Date` >= now()-interval 3 month ";
+$result = mysqli_query($connection,$sql) or die(mysql_error());
+ echo"$sql";
 
- echo"$result"; 
+if (mysqli_num_rows($result) == 0)
+    {
+        echo "Sorry, but we can not find an entry to match your query...<br><br>";
+		 }
+		     else
+    {
+       $all_property = array();  //declare an array for saving property
+//showing property
+echo '<table class="data-table" border="2" width="auto" overflow: "auto" ID="Table1" style="font-size: 70%;">
+        <tr class="data-heading">';  //initialize table tag
+		while ($property = mysqli_fetch_field($result)) {
+    echo '<td>' . $property->name . '</td>';  //get field name for header
+    array_push($all_property, $property->name);  //save those to array
+}
+	}
+	echo '</tr>'; //end tr tag
+//showing all data
+while ($row = mysqli_fetch_array($result)) {
+    echo "<tr>";
+    foreach ($all_property as $item) {
+        echo '<td>' . $row[$item] . '<div style= "width:180px;"</div></td>'; //get items using property value
+    }
+    echo '</tr>';
+	
+}
 }
  ?>
 
